0b8a9b06f614a7ab1a5c822fbd2f3d6fcf2c8039
TEXNO <fbbozorov@gmail.com>
2023-12-03 14:53:57 +0500

    js-vazifa-bool

---
+++ script.js
@@ -0,0 +1,60 @@
+//bool-1
+/*let c;
+let a = 5;
+let b = 5;
+c = a == b;
+console.log(c);*/
+//bool-2
+/*let c;
+let a = 7;
+c= a === 8;
+console.log(c);*/
+//bool-3
+/*let a;
+let b;
+b=5
+a = b>0
+console.log(a);*/
+//bool-4
+/*let a;
+let b;
+b=-5
+a = b<0
+console.log(a);*/
+//bool-5
+/*let a;
+let b;
+a=8
+b=a%2!=0;
+console.log(b);*/
+//bool-6
+/*let a;
+let b;
+a=9
+b=a%2==0;
+console.log(b);*/
+//bool-7
+/*let a;
+let b;
+let c;
+a=5; b=15; c=10
+k=a<b && b<c;
+console.log(k);*/
+//bool-8
+/*let a;
+let b;
+a=12; b=9;
+c=a>0 && b>0;
+console.log(c);*/
+//bool-9
+/*let a;
+let b;
+a=-7; b=-7;
+c=a<0 && b<0;
+console.log(c);*/
+//bool-10
+/*let a;
+let b;
+a=8; b=9;
+c=a%2==0 && b%2==0;
+console.log(c);*/
